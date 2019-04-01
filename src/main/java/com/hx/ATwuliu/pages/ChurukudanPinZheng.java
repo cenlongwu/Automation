254c03609c9d770335f9f38af8485943a7978f12
cenlongwu <71925446@qq.com>
2019-04-01 15:05:41 +0800

    改dataprovider，CSV中文乱码

---
+++ src/main/java/com/hx/ATwuliu/pages/ChurukudanPinZheng.java
@@ -0,0 +1,14 @@
+package com.hx.ATwuliu.pages;
+
+import com.hx.ATwuliu.util.*;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.FindBys;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+//import org.junit.Assert.*;
+
+/**
+ * Created by Administrator on 2019/4/1.
+ */
+public class ChurukudanPinZheng extends Actions {
+}
---
+++ src/main/java/com/hx/ATwuliu/pages/PlanManage.java
@@ -0,0 +1,7 @@
+package com.hx.ATwuliu.pages;
+
+/**
+ * Created by Administrator on 2019/4/1.
+ */
+public class PlanManage {
+}
