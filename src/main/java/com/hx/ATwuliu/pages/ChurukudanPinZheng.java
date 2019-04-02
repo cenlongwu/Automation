cc133907ced3b69b93e5573a79877dd02705e16c
cenlongwu <71925446@qq.com>
2019-04-02 10:03:22 +0800

    4月2号早上，提交代码    --吴岑龙

--- src/main/java/com/hx/ATwuliu/pages/ChurukudanPinZheng.java
+++ src/main/java/com/hx/ATwuliu/pages/ChurukudanPinZheng.java
@@ -11,4 +11,39 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
  * Created by Administrator on 2019/4/1.
  */
 public class ChurukudanPinZheng extends Actions {
+
+    /*************************************************************************************/
+    //页面控件
+
+    /*入库通知书页面*/
+    @FindBy(id="noticeNum")
+    private WebElement Ipt_TongzhishuNum;    //通知书编号输入框
+
+    @FindBy(xpath = "//*[@id=\"app\"]//button[span=\"查 询\"]")
+    private WebElement Btn_Query;     //查询按钮
+
+    @FindBy(xpath = "//*[@id=\"app\"]//div[@class=\"ant-btn-group\"]/button[span=\"新 增\"]")
+    private WebElement Btn_Add;     //新增按钮
+
+    @FindBy(xpath = "//*[@id=\"app\"]//div[@class=\"ant-btn-group\"]/button[span=\"修 改\"]")
+    private WebElement Btn_Modify;     //修改按钮
+
+    @FindBy(xpath = "//*[@id=\"app\"]//div[@class=\"ant-btn-group\"]/button[span=\"删 除\"]")
+    private WebElement Btn_Delete;     //删除按钮
+
+    @FindBy(xpath = "//*[@id=\"app\"]//div[@class=\"ant-btn-group\"]/button[span=\"提 交\"]")
+    private WebElement Btn_Submit;     //提交按钮
+
+    @FindBy(xpath = "//*[@id=\"app\"]//div[@class=\"ant-btn-group\"]/button[span=\"中 止\"]")
+    private WebElement Btn_Suspend;     //中止按钮
+
+    @FindBy(xpath = "//*[@id=\"app\"]//div[@class=\"ant-btn-group\"]/button[span=\"完 成\"]")
+    private WebElement Btn_Complete;     //完成按钮
+
+    @FindBy(xpath = "//*[@id=\"app\"]//div[@class=\"ant-btn-group\"]/button[span=\"导 出\"]")
+    private WebElement Btn_Export;     //导出按钮
+
+    /*************************************************************************************/
+
+
 }
