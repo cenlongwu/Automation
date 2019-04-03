ad6a75df97e5be786a71d0c1d104eacbd7c22ee2
cenlongwu <wucenlong@hxdi.com>
2019-04-03 15:14:12 +0800

    4.3黄

---
+++ src/main/java/com/hx/ATwuliu/pages/DengjiManage.java
@@ -0,0 +1,19 @@
+package com.hx.ATwuliu.pages;
+import com.hx.ATwuliu.util.*;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.FindBys;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+//import org.junit.Assert.*;
+
+/**
+ * Created by Administrator on 2019/4/2.
+ */
+public class DengjiManage extends Actions {
+    /*************************************************************************************/
+    //页面控件
+
+    /*登记管理页面*/
+    @FindBy(id="noticeNum")
+    private WebElement Ipt_TongzhishuNum;    //通知书编号输入框
+}
